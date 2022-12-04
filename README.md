8a09c90db5237d67935a06675013c16d4ec7b8f8
AndyHu8 <huandy01@gmail.com>
2022-12-04 18:25:42 +0100

    Hook up sanity + sanity start problem fix

--- README.md
+++ README.md
@@ -10,10 +10,13 @@
 # Sanity
 ## Muss eingerichtet werden (hier als Clean Project)
 ## sanity docs (Documentation) => Nach Key Wörter suchen, die man nicht versteht
-## sanity manage (Manager, aktuelles Projekt auswählbar)
+## sanity manage (Manager, aktuelles Projekt auswählbar) + (Verbindungsdaten für den Client)
 ## sanity start (Im Ordner "sanity_ecommerce" Schema starten + anmelden => localhost:3333)
 
 # npm i --save-dev @babel/preset-react Problem
 ## Erst die sanity Deps in package.json entfernen => dannn npm i
 ## Danach npm i sanity neu installieren
 ## Danach npm i --save-dev @babel/preset-react installieren
+
+# sanity start Problem
+## Die richtigen Versionen von Deps aus dem Video entnehmen für sanity im sanity_ecommerce (package.json)
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components'
+import { client } from '../lib/client'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner/>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className='products-heading'>
         <h2>Best Selling Product</h2>
@@ -12,12 +13,25 @@ const Home = () => {
       </div>
 
       <div className='products-container'>
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner/>
     </>
   )
+}
+
+//Fetching mit Next.js statt React.js
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'; //Nimm alle Produkte vom Sanity Dashboard
+  const products = await client.fetch(query); //Gehe zum richtigen Client & fetche alle Produkte
+
+  const bannerQuery = '*[_type == "banner"]'; //Nimm alle Banner vom Sanity Dashboard
+  const bannerData = await client.fetch(bannerQuery); //Gehe zum richtigen Client & fetche alle Banner
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
