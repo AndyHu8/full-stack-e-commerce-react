20eb3cc36ed6daa2c48b963266892a72ba89bbc0
AndyHu8 <huandy01@gmail.com>
2022-12-05 21:21:57 +0100

    Layout

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import React from "react"
+import { Layout } from '../components'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} /> {/*Immer das aktuelle Component (z.B. Homepage oder Details Page)*/}
+    </Layout>
+  )
 }
 
 export default MyApp
